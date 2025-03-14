63cf6c78f29b7400d2433d92927cdfb289ab864f
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2025-03-14 18:52:40 +0330

    Fix product skeleton size

--- src/components/product-skeleton.tsx
+++ src/components/product-skeleton.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "./ui/skeleton";
 const ProductSkeleton = () => {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[185px] w-[220px] rounded-xl" />
+      <Skeleton className="h-[290px] w-full rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[220px]" />
-        <Skeleton className="h-4 w-[180px]" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-4/5" />
       </div>
     </div>
   );
