4586b1cfab243aafaaed702532934413120d470f
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2025-03-14 18:57:46 +0330

    Remove images config in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["i.imgur.com", "loremflickr.com", "click.gt", "placeimg.com"],
-  },
 };
 
 export default nextConfig;
